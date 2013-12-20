23194d966d3302bb7f48267daa8d6a958aec2cad
Brian Egger <begger@amchirzrbegger.local>
2013-12-20 12:49:29 -0600

    checkin rename

--- app/controllers/admin.js
+++ app/controllers/admin.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose')
 	, UserModel = mongoose.model('UserModel');
 	
 exports.register = function(req,res){
-	res.render('registration' , {title: "Pawhelp Registration"});
+	res.render('register' , {title: "Pawhelp Registration"});
 };
 
 exports.registersave = function(req,res){
